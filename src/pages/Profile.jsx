021af43efc152177ab731bccaea50a19e3468d30
Hal Chang <jcgk2ndbjy@gmail.com>
2025-05-03 09:06:50 +0800

    將 Profile.jsx 內個人資訊改成米字號

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,9 +4,9 @@ export default function Profile() {
 			<h1 style={{ fontSize: "1.75rem" }}>個人檔案</h1>
 			<h2 style={{ fontSize: "1.25rem" }}>張允豪</h2>
 			<ul>
-				<li style={{ letterSpacing: "1px" }}>手機：0900-348-630</li>
-				<li style={{ letterSpacing: "1px" }}>生日：1999/06/03</li>
-				<li style={{ letterSpacing: "1px" }}>信箱：jcgk2ndbjy@gmail.com</li>
+				<li style={{ letterSpacing: "1px" }}>手機：09**-***-630</li>
+				<li style={{ letterSpacing: "1px" }}>生日：19**/**/**</li>
+				<li style={{ letterSpacing: "1px" }}>信箱：jc********@gmail.com</li>
 			</ul>
 		</div>
 	);
