a02d6f816009122084288ab1c3f5e35264188262
Hal Chang <jcgk2ndbjy@gmail.com>
2025-05-03 08:46:06 +0800

    Fix base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: "/first-react-app/",
+	base: "./",
 	plugins: [react()],
 	server: {
-		historyApiFallback: true, // 網址列手動輸入 /about 或 /contact 子路徑時，都不會 404 Not Found
+		historyApiFallback: true, // 網址列手動輸入 /about 或 /contact 子路徑時，都不會 404 Not Foundㄕ
 	},
 });
